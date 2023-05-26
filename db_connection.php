8734408a94f52c18ed2a540527d177d8bfeac919
ABDULRHMAN MOHAMMED A ALMEHDAR <83496936+AMM48@users.noreply.github.com>
2023-05-26 21:33:19 +0300

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,15 @@
 <?php
 
 // WARNING: NEVER DO THIS IN PRODUCTION
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "todo_db";
-
-$conn = new mysqli($servername, $username, $password, $database);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 // check the connection
 if ($conn->connect_error) {
